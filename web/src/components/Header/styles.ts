98fbb2fcc9392b457619740f36df8d6d5363f0f3
Lucas Barzan <lucasbarzand@gmail.com>
2020-08-06 20:05:44 -0300

    Add web

---
+++ web/src/components/Header/styles.ts
@@ -0,0 +1,42 @@
+import styled, { css } from 'styled-components';
+
+interface ContainerProps {
+  size?: 'small' | 'large';
+}
+
+export const Container = styled.div<ContainerProps>`
+  background: #5636d3;
+  padding: 30px 0;
+
+  header {
+    width: 1120px;
+    margin: 0 auto;
+    padding: ${({ size }) => (size === 'small' ? '0 20px ' : '0 20px 150px')};
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    nav {
+      .selected {
+        border-bottom: 2px solid #ff872c;
+        display: inline-block;
+        line-height: 2.5;
+      }
+
+      a {
+        color: #fff;
+        text-decoration: none;
+        font-size: 16px;
+        transition: opacity 0.2s;
+
+        & + a {
+          margin-left: 32px;
+        }
+
+        &:hover {
+          opacity: 0.6;
+        }
+      }
+    }
+  }
+`;
---
+++ web/src/pages/Dashboard/index.tsx
@@ -0,0 +1,116 @@
+import React, { useState, useEffect } from 'react';
+
+import income from '../../assets/income.svg';
+import outcome from '../../assets/outcome.svg';
+import total from '../../assets/total.svg';
+
+import api from '../../services/api';
+
+import Header from '../../components/Header';
+
+import formatValue from '../../utils/formatValue';
+
+import { Container, CardContainer, Card, TableContainer } from './styles';
+
+interface Transaction {
+  id: string;
+  title: string;
+  value: number;
+  formattedValue: string;
+  formattedDate: string;
+  type: 'income' | 'outcome';
+  category: { title: string };
+  created_at: Date;
+}
+
+interface Balance {
+  income: number;
+  outcome: number;
+  total: number;
+}
+
+const Dashboard: React.FC = () => {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [balance, setBalance] = useState<Balance>({} as Balance);
+
+  useEffect(() => {
+    async function loadTransactions(): Promise<void> {
+      const response = await api.get('/transactions');
+
+      const {
+        transactions: loadedTransactions,
+        balance: loadedBalance,
+      } = response.data;
+
+      console.log(loadedTransactions);
+      console.log(loadedBalance);
+
+      setTransactions(loadedTransactions);
+      setBalance(loadedBalance);
+    }
+
+    loadTransactions();
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <Container>
+        <CardContainer>
+          <Card>
+            <header>
+              <p>Entradas</p>
+              <img src={income} alt="Income" />
+            </header>
+            <h1 data-testid="balance-income">{formatValue(balance.income)}</h1>
+          </Card>
+          <Card>
+            <header>
+              <p>Saídas</p>
+              <img src={outcome} alt="Outcome" />
+            </header>
+            <h1 data-testid="balance-outcome">
+              {formatValue(balance.outcome)}
+            </h1>
+          </Card>
+          <Card total>
+            <header>
+              <p>Total</p>
+              <img src={total} alt="Total" />
+            </header>
+            <h1 data-testid="balance-total">{formatValue(balance.total)}</h1>
+          </Card>
+        </CardContainer>
+
+        <TableContainer>
+          <table>
+            <thead>
+              <tr>
+                <th>Título</th>
+                <th>Preço</th>
+                <th>Categoria</th>
+                <th>Data</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {transactions.map(transaction => (
+                <tr key={transaction.id}>
+                  <td className="title">{transaction.title}</td>
+                  <td className={transaction.type}>
+                    {transaction.type === 'outcome' && '- '}
+                    {formatValue(transaction.value)}
+                  </td>
+                  <td>{transaction.category.title}</td>
+                  <td>{transaction.created_at}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </TableContainer>
+      </Container>
+    </>
+  );
+};
+
+export default Dashboard;
